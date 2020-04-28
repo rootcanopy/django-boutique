011a545cdca102811bcd3e2df49ed34f3ed2b230
rootcanopy <dave.trees201@gmail.com>
2020-04-28 14:41:09 +0200

    debugging urls

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
@@ -8,9 +8,18 @@ from .models import Product
 def all_products(request):
     """ view to show all products, including sorting and search query """
 
-
     products = Product.objects.all()
     context = {
         'products': products,
     }
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ view to show individual product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+    context = {
+        'product': product,
+    }
+    return render(request, 'products/product_detail.html', context)
