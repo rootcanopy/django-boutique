59c513731de398316d807ee366c94f61dc6f9058
rootcanopy <dave.trees201@gmail.com>
2020-04-28 19:26:37 +0200

    added sorting

--- products/views.py
+++ products/views.py
@@ -20,12 +20,13 @@ def all_products(request):
 
     if request.GET:
         if 'sort' in request.GET:
-            sort = request.GET['sort']
+            sortkey = request.GET['sort']
             sort = sortkey
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
