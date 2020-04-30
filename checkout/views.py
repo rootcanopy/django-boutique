ae7da52d8d6409f6cbfc1fa1507b92a2b3316712
rootcanopy <dave.trees201@gmail.com>
2020-04-30 23:01:16 +0200

    stripe works

--- checkout/views.py
+++ checkout/views.py
@@ -23,17 +23,23 @@ def checkout(request):
     stripe.api_key = stripe_secret_key
     intent = stripe.PaymentIntent.create(
         amount=stripe_total,
-        curreny=settings.STRIPE_CURRENCY,
+        currency=settings.STRIPE_CURRENCY,
     )
 
     print(intent)
 
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. Did you forget to set it in your enviroment?')
+
+
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
         'stripe_public_key': 'stripe_public_key',
-        'client_secret': 'test client secret',
+        'client_secret': 'intent.client_secret',
 
     }
     return render(request, template, context)
