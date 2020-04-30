ed4a7e73ebf91028edd96ba62e4b2d12534d5757
rootcanopy <dave.trees201@gmail.com>
2020-04-30 16:37:36 +0200

    errors in checkout

--- checkout/views.py
+++ checkout/views.py
@@ -18,10 +18,10 @@ def checkout(request):
         return redirect(reverse('products'))
     
     current_bag = bag_contents(request)
-    total = current_bag['grand-total']
-    stripe_total = round(total * 180)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
     stripe.api_key = stripe_secret_key
-    stripe.PaymentIntent.create(
+    intent = stripe.PaymentIntent.create(
         amount=stripe_total,
         curreny=settings.STRIPE_CURRENCY,
     )
@@ -32,7 +32,7 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_C9HJQ1QFL2S9E9LgiOW3oXDq00PPJmu2U5',
+        'stripe_public_key': 'stripe_public_key',
         'client_secret': 'test client secret',
 
     }
