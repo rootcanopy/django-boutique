9c11d1ed0a1fc77a629209f86051eb3a5daa2a7a
rootcanopy <dave.trees201@gmail.com>
2020-05-01 16:20:09 +0200

    works

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,3 +1,4 @@
+// checkout stripe box bottom
 var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
 var clientSecret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripePublicKey);
